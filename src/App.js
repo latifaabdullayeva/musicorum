0dedd3cbab9fae77035142a30e8b97fd13a3e949
Latifa <latifaabdullayeva@gmail.com>
2020-11-02 13:18:11 +0100

    Changes style of index

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+function App() {
+    const classes = useStyles();
+
+    function FormRow() {
+        return (
+            <React.Fragment>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>item</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>item</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>item</Paper>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
+    return (
+        <div className="Charts">
+            <Grid container spacing={12}>
+                <Grid item xs={12}>
+                    <Paper>xs=12</Paper>
+                </Grid>
+                <Grid item xs={2}>
+                    <Paper>xs=2</Paper>
+                </Grid>
+                <Grid item xs={10} cellHeight={180}>
+                    <Grid container item xs={12} spacing={3}>
+                        <FormRow/>
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <FormRow/>
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <FormRow/>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default App;
