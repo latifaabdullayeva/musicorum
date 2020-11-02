97ba2a8a4beb32f8194f7cc0be984dc51b0759ae
Latifa <latifaabdullayeva@gmail.com>
2020-11-02 20:15:31 +0100

    Adds lists of top playlists

--- src/components/TopLists/TopLists.js
+++ src/components/TopLists/TopLists.js
@@ -1,39 +1,9 @@
-import '../../App.css';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import React from "react";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-}));
+import FormRow from "../FormRow/FormRow";
 
 function TopLists() {
-    const classes = useStyles();
-
-    function FormRow() {
-        return (
-            <React.Fragment>
-                <Grid item xs={4}>
-                    <Paper className={classes.paper}>item</Paper>
-                </Grid>
-                <Grid item xs={4}>
-                    <Paper className={classes.paper}>item</Paper>
-                </Grid>
-                <Grid item xs={4}>
-                    <Paper className={classes.paper}>item</Paper>
-                </Grid>
-            </React.Fragment>
-        );
-    }
-
     return (
         <div className="Charts">
             <Grid container spacing={12}>
