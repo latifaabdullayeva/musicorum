9a0ad4e7e455b7ce6860b0030c52be9e56859ee0
Latifa <latifaabdullayeva@gmail.com>
2020-11-03 00:15:11 +0100

    Adds title of top 20 playlists

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, {Component} from "react";
 import Grid from "@material-ui/core/Grid";
-import FormRow from "../FormRow/FormRow";
+import CardElement from "../CardElement/CardElement";
 
-function Albums() {
-    return (
-        <div>
-            <h1>Top Albums</h1>
-            <Grid container spacing={1}>
-                <FormRow/>
-            </Grid>
-        </div>
-    )
+function getAlbums() {
+    return fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/playlists?limit=20')
+        .then(res => {
+            if (res.ok) {
+                console.log('SUCCESS');
+                return res.json()
+            } else {
+                console.log('FAILURE');
+                throw new Error("Error res.ok false")
+            }
+        })
+        .catch(error => console.log('ERROR' + error.message));
+}
+
+class Albums extends Component {
+    state = {
+        albumList: []
+    }
+
+    componentDidMount() {
+        getAlbums().then(data => {
+            let albumTitles = [];
+            for (let i = 0; i < data.data.length; i++) {
+                albumTitles.push(data.data[i].title);
+            }
+            this.setState({albumList: albumTitles})
+        })
+    }
+
+    render() {
+        let albumList = this.state.albumList;
+        return (
+            <div>
+                <h1>Top Albums</h1>
+                <Grid container spacing={1}>
+                    {albumList.map(album => {
+                        return (
+                            <Grid item xs={3} key={album}>
+                                <CardElement key={album} title={album}/>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default Albums;
--- src/components/CardElement/CardElement.js
+++ src/components/CardElement/CardElement.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -17,7 +15,7 @@ const useStyles = makeStyles({
     },
 });
 
-function CardElement() {
+function CardElement(title) {
     const classes = useStyles();
 
     return (
@@ -30,7 +28,7 @@ function CardElement() {
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
+                        {title.title}
                     </Typography>
                 </CardContent>
             </CardActionArea>
