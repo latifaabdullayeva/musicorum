810d243aa99b8186a6d5c768d33929dc514bf440
Latifa <latifaabdullayeva@gmail.com>
2020-11-03 20:28:14 +0100

    Adds SideMenu

---
+++ src/components/SideMenu/SideMenu.js
@@ -0,0 +1,31 @@
+import React from "react";
+import MusicControlCard from "./MusicControl/MusicControl";
+import Grid from "@material-ui/core/Grid";
+import SideMenuList from "./SideMenuList/SideMenuList";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        borderRight: '0.1em solid rgba(0,0,0,0.2)',
+        height: '45rem',
+        flexDirection: 'column',
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'block',
+        },
+    },
+}));
+
+
+function SideMenu() {
+    const classes = useStyles();
+
+    return (
+        <Grid className={classes.root}>
+            <SideMenuList/>
+            <MusicControlCard style={{flex: '0'}}/>
+        </Grid>
+    )
+}
+
+export default SideMenu;
