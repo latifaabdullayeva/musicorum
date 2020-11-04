2b9f7706c00b16f6eeed329cdf050803d58b97d5
Latifa <latifaabdullayeva@gmail.com>
2020-11-04 21:40:47 +0100

    Adds Favorite button to ArtistElement.js

--- src/components/Artists/ArtistElement.js
+++ src/components/Artists/ArtistElement.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles({
     root: {
@@ -14,24 +18,44 @@ const useStyles = makeStyles({
         height: 0,
         paddingTop: '100%',
         borderRadius: '8px',
-    },
+    }
 });
 
 function ArtistElement(props) {
+
     const classes = useStyles();
+
+    const [background, setBackground] = useState("null");
+
+    const clickHandler = () => {
+        setBackground("#fd9e7f")
+    }
+    const styles = {
+        color: background
+    };
+
     return (
         <Card className={classes.root}>
             <CardActionArea style={{borderRadius: '8px'}}>
-                <CardMedia className={classes.media}
-                           image={props.cover}
-                           src={props.cover}
-                           name={props.name}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="body1" component="span">
-                        {props.name}
-                    </Typography>
-                </CardContent>
+                <Link to="/artistTracks" style={{textDecoration: 'none', color: 'black'}}>
+                    <CardMedia className={classes.media}
+                               image={props.cover}
+                               src={props.cover}
+                               name={props.name}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="body1" component="span">
+                            {props.name}
+                        </Typography>
+                    </CardContent>
+                </Link>
+                <IconButton aria-label="add to favorites"
+                            onClick={clickHandler} style={styles}>
+                    <FavoriteIcon/>
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon/>
+                </IconButton>
             </CardActionArea>
         </Card>
     );
