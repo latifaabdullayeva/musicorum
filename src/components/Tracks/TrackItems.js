75db92cde18bf00972b53904fdaa262e983901c6
Latifa <latifaabdullayeva@gmail.com>
2020-11-05 23:55:38 +0100

    Adds TrackItems.js

---
+++ src/components/Tracks/TrackItems.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import MusicControlCard from "../SideMenu/MusicControl/MusicControl";
+
+const useStyles = makeStyles({
+    root: {
+        margin: '1rem',
+    },
+    media: {
+        height: 0,
+        paddingTop: '100%',
+        borderRadius: '8px',
+    }
+});
+
+function TrackItems(props) {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea style={{borderRadius: '8px'}}>
+                {/*<Link to="/tracks" style={{textDecoration: 'none', color: 'black'}}>*/}
+                <CardMedia className={classes.media}
+                           image={props.cover}
+                           src={props.cover}
+                           title={props.title}
+                           rank={props.rank}
+                           preview={props.preview}
+                />
+                <GridListTileBar title={props.title}
+                                 subtitle={<span>by: {props.artistName} </span>}/>
+                {/*</Link>*/}
+            </CardActionArea>
+            <div style={{position: 'fixed', bottom: '0', left: '0', width: '100%'}}>
+                <MusicControlCard
+                    preview={props.preview}
+                    title={props.title}
+                    artistName={props.artistName}
+                />
+            </div>
+        </Card>
+    );
+}
+
+export default TrackItems;
