a65e79d1b73ca9d5a8971ece6d7d278f85be8438
Latifa <latifaabdullayeva@gmail.com>
2020-11-05 23:52:18 +0100

    Adds ChartPlaylistItems.js

--- src/components/Charts/ChartPlaylistItems.js
+++ src/components/Charts/ChartPlaylistItems.js
@@ -4,8 +4,6 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
-import IconButton from "@material-ui/core/IconButton";
-import StarBorderIcon from "@material-ui/icons/StarBorder";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,35 +18,30 @@ const useStyles = makeStyles((theme) => ({
         transform: 'translateZ(0)',
     },
     title: {
-        color: theme.palette.primary.light,
+        color: '#fd9e7f',
     },
     titleBar: {
-        background:
-            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+        background: 'rgba(0,0,0,0.5)',
     },
 }));
 
 function ChartPlaylistItems(props) {
     const classes = useStyles();
 
-    if (props.chartList.length > 0) {
+    if (props.chartPlaylist.length > 0) {
         return (
             <div className={classes.root}>
                 <GridList className={classes.gridList} cols={2.5}>
-                    {props.chartList.map((chartList) => (
-                        <GridListTile key={chartList.coverImage}>
-                            <img src={chartList.coverImage} alt={chartList.title}/>
+                    {props.chartPlaylist.map((chartPlaylist) => (
+                        <GridListTile key={chartPlaylist.coverImage} style={{width: '20%'}}>
+                            <img src={chartPlaylist.coverImage} alt={chartPlaylist.title}/>
                             <GridListTileBar
-                                title={chartList.title}
+                                title={chartPlaylist.title}
+                                subtitle={chartPlaylist.trackNums + ' tracks'}
                                 classes={{
                                     root: classes.titleBar,
                                     title: classes.title,
                                 }}
-                                actionIcon={
-                                    <IconButton aria-label={`star ${chartList.title}`}>
-                                        <StarBorderIcon className={classes.title}/>
-                                    </IconButton>
-                                }
                             />
                         </GridListTile>
                     ))}
