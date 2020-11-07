95d83d334e1db2cddc1bbce39afffbae5c48adeb
Latifa <latifaabdullayeva@gmail.com>
2020-11-07 10:49:41 +0100

    Removed SearchBar from ToolBar

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import {fade, makeStyles} from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from "@material-ui/core/IconButton";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import MLogoTransparent from '../../MLogoTransparent.png';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,49 +14,8 @@ const useStyles = makeStyles((theme) => ({
     },
     title: {
         flexGrow: 1,
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
+        display: 'block',
+        position: 'center'
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -83,22 +42,15 @@ function ToolBar(props) {
                     >
                         <MenuIcon/>
                     </IconButton>
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        LOGO
+                    <Typography className={classes.title} variant="body1" noWrap>
+                        <img src={MLogoTransparent} alt={'logo'}
+                             style={{height: '2rem',}}/>
                     </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon/>
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{'aria-label': 'search'}}
-                        />
-                    </div>
+                    <IconButton aria-label="display more actions" edge="end" color="inherit">
+                        <Avatar alt="Me"
+                                src="https://avatars2.githubusercontent.com/u/17601156?s=460&u=ea34b8affe8b8ec0a95fc74ca65b3cf1ab82137a&v=4"/>
+                    </IconButton>
+
                 </Toolbar>
             </AppBar>
         </div>
